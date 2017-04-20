09e089003c84df01816f5c09629c6d551bdac800
村瀬 大和 <muraseyamato@murasenoiMac.local>
2017-04-20 12:22:39 +0900

    テーマの変更

--- docs/conf.py
+++ docs/conf.py
@@ -83,12 +83,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'haiku'
-# html_theme = 'sphinx_rtd_theme'
+
+html_theme = 'pyramid'
 # テーマの widthを開放
 # html_style = 'css/my_theme.css'
-
-
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
