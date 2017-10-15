052fc13a7ba820c8574b37fc14ac7ae47d3f7722
RossyWhite <daikishiroi@gmail.com>
2017-10-15 23:40:21 +0900

    Add translate of public and trade

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ release = 'v1.1.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 gettext_compact = False
 locale_dirs = ['locale']
 
